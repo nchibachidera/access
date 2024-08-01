4c19da6c4d04ce36f96585e5aa6401faa22b7471
N.DERAJ <nchibachidera0@gmail.com>
2024-08-01 10:07:17 -0700

    initial commit

--- index2.js
+++ index2.js
@@ -2,12 +2,14 @@ import express from "express";
 import superheroes from "superheroes";
 
 const app = express();
+const port = 3000;
+
 
 app.get("/", (req, res) => {
-    let name = superheroes.random();
-    res.send('<h1>Behold, I am ${name}!</h1>');
+  let name = superheroes.random();
+  res.send(`<h1>Behold, I am ${name}!</h1>`);
 });
 
-app.listen(3000, () => {
-    console.log('server runing on port 3000.');
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
